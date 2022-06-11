a76940724d8cb906d73126edad4ddec921624744
Facundo Torreyra <fgtgerman@gmail.com>
2022-06-11 09:48:13 -0300

    Delete y descuento

--- index.js
+++ index.js
@@ -7,24 +7,26 @@ buttonBuy.forEach(buttonCard => {
 const buttonDelete = document.querySelectorAll('.productItem__delete');
 buttonDelete.forEach(del => {
     del.addEventListener('click', () => deleteProduct(del.value));
-})
+});
 
 const buttonFactu = document.querySelector('.buttonFactu');
 buttonFactu.addEventListener('click', () => buttonReceipt());
 
-
-let cantidad = 0;
-let totalElementos = [];
 const prices = {
     Pasta: 450,
     Miel: 670,
     Oliva: 360,
-}
-
+};
 const factu = {};
+const cuponsDiscount = ['descuento1', 'descuento2', 'descuento3', 'descuento4'];
 
+let cantidad = 0;
+let totalElementos = [];
 let totalProducto = 0;
 let totalGeneral = 0;
+let isDiscount = false;
+let deleteBoolean = true;
+
 
 function addProduct(value) {
     cantidad = parseInt(prompt('Ingrese cuantos quiere comprar'));
@@ -39,19 +41,44 @@ function addProduct(value) {
 }
 
 function buttonReceipt() {
+    if (deleteBoolean) {
+        descuentos();
+    };
     alert('Para ver la factura abra la consola');
     let i = 0;
     Object.keys(factu).map(key => {
         const value = factu[key];
-        console.log(`Producto: ${key} - Cantidad: ${totalElementos[i]} - Total: ${value}`)
+        console.log(`Producto: ${key} - Cantidad: ${totalElementos[i]} - Total: ${value}`);
         i++;
 
     })
-    console.log(`Total General: ${totalGeneral} `)
+    if (isDiscount) {
+        const totalDiscount = totalGeneral - totalGeneral * 10 / 100;
+        console.log(`Total General: ${totalGeneral}`);
+        console.log('Descuento: 10%');
+        console.log(`Total con descuento: ${totalDiscount}`);
+    } else {
+        console.log(`Total General: ${totalGeneral}`)
+    }
+    deleteBoolean = true;
+}
+
+
+function descuentos() {
+    const codeDiscount = prompt('Ingrese código de descuento si tiene');
+    isDiscount = cuponsDiscount.some((el) => el == codeDiscount);
+    if (isDiscount) {
+        console.log('Codigo correcto');
+        const indexCode = cuponsDiscount.indexOf(codeDiscount);
+        cuponsDiscount.splice(indexCode, 1);
+    } else {
+        console.log('El código no es válido');
+    }
 }
 
 
 function deleteProduct(value) {
+    deleteBoolean = false;
     Object.entries(factu).forEach(val => {
         if (value === val[0]) {
             delete factu[val[0]];
